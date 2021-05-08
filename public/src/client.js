be776d40a7b5fee36c7cf5f946966bbee62795e2
Taylor Harding <softwareisharding@gmail.com>
2021-05-07 20:41:41 -0700

    form now submits user's name to server

--- public/src/client.js
+++ public/src/client.js
@@ -1,5 +1,15 @@
 let socket = io();
 
-socket.emit('user-enter', "anonymous");
-
 console.log("Welcome to Americana!");
+
+$('#entrance-form').submit(function(e){
+    e.preventDefault(); 
+    let username = $('#name-textbox').val();
+    console.log(username);
+    if (username != '') {
+        socket.emit('user-enter', username);
+        console.log('emitting user-enter with username: ' + username);
+        $('#name-textbox').val('');
+        return false;
+    }
+});
